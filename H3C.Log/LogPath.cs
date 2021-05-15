004082c846e0e2d4a01c6754de0ce2e9e213c274
yudong <1016724958@qq.com>
2021-05-15 17:37:14 +0800

    添加LogFolder

---
+++ H3C.Log/LogPath.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kybs0.Log
+{
+    /// <summary>
+    /// 日志路径
+    /// </summary>
+    public class LogPath
+    {
+        private readonly string _appName;
+
+        public LogPath(string appName)
+        {
+            _appName = appName;
+        }
+        /// <summary>
+        /// 提示日志路径
+        /// </summary>
+        public string InfoPath=> GetLogFilePath($"info_{DateTime.Now:yyyy-MM-dd}.txt");
+        /// <summary>
+        /// 异常日志路径
+        /// </summary>
+        public string ErrorPath=> GetLogFilePath($"error_{DateTime.Now:yyyy-MM-dd}.txt");
+        /// <summary>
+        /// 功能日志路径
+        /// </summary>
+        public string MessagePath => GetLogFilePath($"message_{DateTime.Now:yyyy-MM-dd}.txt");
+        private string GetLogFilePath(string fileName)
+        {
+            string logFolder = GetLogFolder();
+            string logFilePath = Path.Combine(logFolder, fileName);
+
+            try
+            {
+                if (!File.Exists(logFilePath))
+                {
+                    var aaa = File.Create(logFilePath);
+                    aaa.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return logFilePath;
+        }
+
+        private string _logFolder = string.Empty;
+        public string GetLogFolder()
+        {
+            if (!string.IsNullOrWhiteSpace(_logFolder))
+            {
+                return _logFolder;
+            }
+            var dataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var logFolder = Path.Combine(dataFolder, _appName,"Log");
+            CreateFolder(logFolder);
+            if (Directory.Exists(logFolder))
+            {
+                _logFolder = logFolder;
+            }
+            return _logFolder;
+        }
+        public void CreateFolder(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+    }
+}
---
+++ H3C.Log/Logger.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Kybs0.Log
+{
+    public class Logger
+    {
+        private readonly LogPath _logPath;
+
+        public Logger(string appName)
+        {
+            _logPath = new LogPath(appName);
+        }
+
+        public string LogFolder => _logPath.GetLogFolder();
+
+        #region 记录日志
+
+        public void Info(string info)
+        {
+            try
+            {
+                var infos = new List<string>()
+                {
+                    $"{DateTime.Now:yyyy-MM-dd hh:mm:ss}: {info}"
+                };
+                File.AppendAllLines(_logPath.InfoPath, infos);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        public void Message(string message)
+        {
+            try
+            {
+                var infos = new List<string>()
+                {
+                    "*********************************************************************",
+                    $"时间:{DateTime.Now:yyyy-MM-dd hh:mm:ss}",
+                    $"描述:{message}\r\n",
+                };
+                File.AppendAllLines(_logPath.MessagePath, infos);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        public void Error(string error)
+        {
+            try
+            {
+                var infos = new List<string>()
+                {
+                    "*********************************************************************",
+                    $"时间:{DateTime.Now:yyyy-MM-dd hh:mm:ss}",
+                    $"描述:{error}\r\n",
+                };
+                File.AppendAllLines(_logPath.ErrorPath, infos);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        public void Error(string error, Exception ex)
+        {
+            try
+            {
+                var infos = new List<string>()
+                {
+                    "*********************************************************************",
+                    $"时间:{DateTime.Now:yyyy-MM-dd hh:mm:ss}",
+                    $"描述:{error}",
+                    $"{ex.Message}"
+                };
+                if (ex.StackTrace != null)
+                {
+                    infos.Add($"{ex.StackTrace}\r\n");
+                }
+                else
+                {
+                    infos[infos.Count - 1] += "\r\n";
+                }
+                File.AppendAllLines(_logPath.ErrorPath, infos);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+        public void Error(Exception ex)
+        {
+            try
+            {
+                var infos = new List<string>()
+                {
+                    "*********************************************************************",
+                    $"时间:{DateTime.Now:yyyy-MM-dd hh:mm:ss}",
+                    $"{ex.Message}",
+                };
+                if (ex.StackTrace != null)
+                {
+                    infos.Add($"{ex.StackTrace}\r\n");
+                }
+                else
+                {
+                    infos[infos.Count - 1] += "\r\n";
+                }
+                File.AppendAllLines(_logPath.ErrorPath, infos);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        #endregion
+    }
+}

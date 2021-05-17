53477db00bbf0a3de39749324f5cd98b9fc630ab
yudong <1016724958@qq.com>
2021-05-17 17:10:00 +0800

    添加文件

---
+++ Kybs0.Log/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kybs0.Log
+{
+    public interface ILogger
+    {
+        string LogFolder { get; }
+        void Info(string info);
+        void Message(string message);
+        void Error(string error);
+        void Error(string error, Exception ex);
+        void Error(Exception ex);
+    } 
+}
